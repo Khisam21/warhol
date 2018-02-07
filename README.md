9276320baa4294a33e470a6a2af1ba6222310a27
Khisam21 <35201442+Khisam21@users.noreply.github.com>
2018-02-07 10:14:28 +0300

    Rename README.md to  warhol.py

--- README.md
+++
@@ -1,3 +0,0 @@
-from PIL import Image
-img=Image.open('C:\Users\Равиль\warhol\marlin.jpg')
-img.show()
---
+++ warhol.py
@@ -0,0 +1,3 @@
+from PIL import Image
+img=Image.open('C:\Users\Ðàâèëü\warhol\marlin.jpg')
+img.show()
