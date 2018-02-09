c2a54031b2c67a0905addec1ef9789b377b9880a
Khisam21 <35201442+Khisam21@users.noreply.github.com>
2018-02-09 15:23:48 +0300

    Update warhol.py

--- warhol.py
+++ warhol.py
@@ -1,3 +1,9 @@
 from PIL import Image
-img=Image.open('C:\Users\Ðàâèëü\warhol\marlin.jpg')
+img=Image.new('RGB',(1473,1440),'black')
+img1=Image.open('C:\Users\Равиль\warhol\marlin.jpg')
+img.paste(img1,(0,0),img1.im)
+
+img.save("img_with_watermark.png")          
 img.show()
+
+
